80f2e9ae0b29d13e3994b45fb6873c0fc43aa347
Tristan Bastian <tristan.bastian@softwareag.com>
2021-02-13 14:17:39 +0100

    initial typescript implementation of storage plugin

--- backend/data/node-red-c8y-storage-plugin/src/index.ts
+++ backend/data/node-red-c8y-storage-plugin/src/index.ts
@@ -1,18 +1,18 @@
-const constants = require("./constants");
-const C8yHandler = require("./C8yHandler");
+import { defaultCollectionNames } from './constants';
+import { C8yHandler } from './C8yHandler';
 
 
-var settings;
-var c8yHandler;
+let settings;
+let c8yHandler: C8yHandler;
 
-var storageModule = {    
-    init: function(_settings) {
+const storageModule = {
+    init(_settings: any) {
         settings = _settings;
         if (
-            settings.storageModuleOptions == null || 
-            settings.storageModuleOptions.c8yUrl == null || 
-            settings.storageModuleOptions.tenant == null || 
-            settings.storageModuleOptions.user == null || 
+            settings.storageModuleOptions == null ||
+            settings.storageModuleOptions.c8yUrl == null ||
+            settings.storageModuleOptions.tenant == null ||
+            settings.storageModuleOptions.user == null ||
             settings.storageModuleOptions.password == null ||
             settings.storageModuleOptions.applicationId == null) {
             throw new Error("c8y storage module's required parameters are not defined");
@@ -21,53 +21,54 @@ var storageModule = {
         // setup environment variables to use for mqtt
         process.env.MQTT_USER = process.env.C8Y_TENANT + '/' + process.env.C8Y_USER;
         process.env.MQTT_PASSWORD = process.env.C8Y_PASSWORD;
+        // tslint:disable-next-line: no-console
         console.log('MQTT BASEURL: ', process.env.C8Y_BASEURL_MQTT);
 
-        this.collectionNames = Object.assign(constants.DefaultCollectionNames);
+        this.collectionNames = Object.assign(defaultCollectionNames);
         if(settings.storageModuleOptions.collectionNames != null){
-            for(let settingsColName of Object.keys(settings.storageModuleOptions.collectionNames)){
+            for(const settingsColName of Object.keys(settings.storageModuleOptions.collectionNames)){
                 this.collectionNames[settingsColName] = settings.storageModuleOptions.collectionNames[settingsColName];
             }
         }
-                
+
         c8yHandler = new C8yHandler(settings.storageModuleOptions.c8yUrl, settings.storageModuleOptions.tenant, settings.storageModuleOptions.user, settings.storageModuleOptions.password, settings.storageModuleOptions.applicationId);
         return c8yHandler.connect();
     },
 
-    getFlows: function() {
+    getFlows() {
         return c8yHandler.findAll(this.collectionNames.flows);
     },
 
-    saveFlows: function(flows) {
+    saveFlows(flows: any) {
         return c8yHandler.saveAll(this.collectionNames.flows, flows);
     },
 
-    getCredentials: function() {
+    getCredentials() {
         return c8yHandler.getFromTenantOptions(this.collectionNames.credentials, true);
     },
 
-    saveCredentials: function(credentials) {
+    saveCredentials(credentials: any) {
         return c8yHandler.saveInTenantOptions(this.collectionNames.credentials, credentials, true);
     },
 
-    getSettings: function() {
+    getSettings() {
         return c8yHandler.getAsOne(this.collectionNames.settings);
     },
-    saveSettings: function(settings) {
-        return c8yHandler.saveAsOne(this.collectionNames.settings, settings);
+    saveSettings(newSettings: any) {
+        return c8yHandler.saveAsOne(this.collectionNames.settings, newSettings);
     },
-    getSessions: function() {
+    getSessions() {
         return c8yHandler.findAll(this.collectionNames.sessions);
     },
-    saveSessions: function(sessions) {
+    saveSessions(sessions: any) {
         return c8yHandler.saveAll(this.collectionNames.sessions, sessions);
     },
 
-    getLibraryEntry: function(type, path) {
+    getLibraryEntry(type: any, path: any) {
         return c8yHandler.findOneByPath(type, path);
     },
 
-    saveLibraryEntry: function(type, path, meta, body) {
+    saveLibraryEntry(type: any, path: any, meta: any, body: any) {
         return c8yHandler.saveOrUpdateByPath(type, path, meta, body) ;
     }
 };
