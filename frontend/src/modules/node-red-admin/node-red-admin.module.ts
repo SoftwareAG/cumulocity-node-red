a40f67af6d385f9813eaacaec0f54b4e85d8c280
Tristan Bastian <tristan.bastian@softwareag.com>
2023-09-25 21:30:26 +0200

    [no-issue] upgrade to version 1018 of WebSDK (#149)

--- frontend/src/modules/node-red-admin/node-red-admin.module.ts
+++ frontend/src/modules/node-red-admin/node-red-admin.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from "@angular/core";
 import { NodeRedIframeModule } from "@modules/node-red-iframe/node-red-iframe.module";
 import {
   CoreModule,
-  gettext,
-  HOOK_NAVIGATOR_NODES,
-  HOOK_ROUTE,
-  HOOK_TABS,
-  NavigatorNodeData,
-  Route,
+  hookNavigator,
+  hookRoute,
+  hookTab,
 } from "@c8y/ngx-components";
 import { NodeRedIframeComponent } from "@modules/node-red-iframe/node-red-iframe.component";
 import { NodeRedAdminTabFactory } from "./admin-tab.factory";
@@ -17,31 +14,17 @@ import { NodeRedAdminGuard } from "./admin.guard";
   imports: [CoreModule, NodeRedIframeModule],
   declarations: [],
   providers: [
-    {
-      provide: HOOK_ROUTE,
-      useValue: [
-        {
-          path: "node-red/admin",
-          component: NodeRedIframeComponent,
-          data: {
-            src: "/service/node-red/",
-          },
-          // canActivate: [NodeRedAdminGuard],
-          // tabs: [{label: 'test', icon: 'rocket', path: 'node-red/admin'}]
-        },
-      ] as Route[],
-      multi: true,
-    },
-    {
-      provide: HOOK_TABS,
-      useClass: NodeRedAdminTabFactory,
-      multi: true,
-    },
-    {
-      provide: HOOK_NAVIGATOR_NODES,
-      useClass: NodeRedAdminGuard,
-      multi: true,
-    }
+    hookRoute({
+      path: "node-red/admin",
+      component: NodeRedIframeComponent,
+      data: {
+        src: "/service/node-red/",
+      },
+      // canActivate: [NodeRedAdminGuard],
+      // tabs: [{label: 'test', icon: 'rocket', path: 'node-red/admin'}]
+    }),
+    hookTab(NodeRedAdminTabFactory),
+    hookNavigator(NodeRedAdminGuard)
   ],
 })
 export class NodeRedAdminModule {}
