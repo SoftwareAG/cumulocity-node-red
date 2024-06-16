f7b689e4b4ab4b3a835433c1e06c99b6db8b8491
Tristan Bastian <tristan.bastian@softwareag.com>
2024-06-16 21:09:22 +0200

    feat: upgrade to angular 17 and websdk 1020

--- cumulocity-node-red-ui/src/main.ts
+++ cumulocity-node-red-ui/src/main.ts
@@ -1,18 +1,17 @@
-import { applyOptions, loadOptions, loginOptions } from '@c8y/bootstrap';
+import { applyOptions, loadOptions } from '@c8y/bootstrap';
 
-const barHolder: HTMLElement = document.querySelector('body > .init-load');
-export const removeProgress = () => barHolder && barHolder.parentNode.removeChild(barHolder);
+const barHolder: HTMLElement | null = document.querySelector('body > .init-load');
+export const removeProgress = () => barHolder && barHolder.parentNode?.removeChild(barHolder);
 
 applicationSetup();
 
 async function applicationSetup() {
   const options = await applyOptions({
-    ...(await loadOptions()),
-    ...((await loginOptions()) as object)
+    ...(await loadOptions())
   });
 
   const mod = await import('./bootstrap');
   const bootstrapApp = mod.bootstrap || (window as any).bootstrap || (() => null);
 
-  return Promise.resolve(bootstrapApp(options)).then(removeProgress);
+  return Promise.resolve(bootstrapApp()).then(removeProgress);
 }
